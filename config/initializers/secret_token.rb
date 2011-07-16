df63d68d865f5aa14a80250d1158b7479272783e
Gautam Kedia <gautamkedia@gmail.com>
2011-07-16 15:06:36 -0700

    Empty Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birthdaygofer::Application.config.secret_token = '3375eedb4d182c5a9c711d14a583c95bdad2d3c4cc7db4806c999a5375daf21b7b17666d771ce6e2cf50ec84f64b82b37c5948cc6662b313e5fce333750a7392'
